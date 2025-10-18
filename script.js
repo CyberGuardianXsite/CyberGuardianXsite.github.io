295b2d15df8b4b3b1a0305b30e2bdce867aa9a8b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-18 15:17:34 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function innerHTML(p1, p2) {
+  document.getElementById(p1).innerHTML = p2
+}
+function TextContent(p1, p2) {
+  document.getElementById(p1).textContent = p2
+}
+TextContent('iframe1', '<iframe src=\"https://CyberGuardianXsite.github.io/Проект%20(13)\"></iframe>')
